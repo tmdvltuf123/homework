36d9ef29f293e5518755b4ca99d6eb48e88d2000
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-03-17 21:45:17 +0900

    2주차 과제 제출

---
+++ helloworld.py
@@ -0,0 +1,30 @@
+#number = 2 + 3 * 4
+#number = number + 2
+#print("숫자의 값은 " + str(number) + " 입니다")
+
+#print(abs(-5))  숫자의 절댓값
+#print(pow(4, 2))  4의 제곱
+#print(max(5, 12)) max값 12
+#print(min(5, 12)) min값 5
+#print(round(3.14))  3.14 반 올림 --> 3
+
+#from random import *
+#print(randint(1, 45)) 1 이상 45이하 랜덤 수
+#date = randint(4, 28)
+#print("스터디 일정은" + str(date) + "일 입니다")
+
+#sentence = "나는 소년입니다"
+#print(sentence)
+
+#jumin = "020918-1234567"
+#print("성별 :" + jumin[7])  #슬라이싱 0123456 7번째칸 숫자 지목
+#print("연 : " + jumin[0:2])  #슬라이싱 [0:2] 뜻이 뭐냐면 0번쨰부터 2번쨰 직전까지 0, 2  (o) / 0 (x) 
+#print("뒤 7자리 뒤에서부터" + jumin[-7:])
+
+python = "Python is Amazing"
+#print(python.lower())  #전부 소문자로
+#print(python.upper())  #전부 대문자로
+#print(python[0].isupper()) #0번째 자리가 대문자 인지 물어보는 함수
+#print(len(python))  #파이썬 함수 문자 길이 띄어쓰기도 길이에 포함
+print(python.replace("Python", "Java")) #문장 안에 있는 특정값을 바꾸기
+#나도 코딩 파이썬 기본편 58 23초
