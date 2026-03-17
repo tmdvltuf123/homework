70bfaa800c1bd4b07388cd715d77e34cafecfdf6
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-03-17 22:56:21 +0900

    2주차 과제

--- kgToPound.py
+++ kgToPound.py
@@ -1,6 +1,6 @@
 input_kg = float(input("몇 킬로그램을 입력하실 것인가요?"))
 output_lb = 2.20462 * input_kg
-print("해당 킬로그램을 파운드로 변환시킨 결과:", output_lb, "lb 입니다")
+print("해당 킬로그램을 파운드로 변환시킨 결과:", output_lb, "lb 입니다.")
 
 
 
