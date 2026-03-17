fbb0d64eb34b3c9cf33903ee492ec5f2896a15df
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-03-17 22:15:34 +0900

    2주차 과제 제출

---
+++ CircleCal.py
@@ -0,0 +1,6 @@
+pi = 3.14
+input_r = float(input("원의 반지름은 몇cm 인가요? "))
+output_c = input_r * 2 * pi
+output_a = (input_r **2) * pi
+print("해당원의 둘레는: ", output_c , "cm 입니다.")
+print("해당원의 넓이는: ", output_a , "cm^2 입니다. ")
