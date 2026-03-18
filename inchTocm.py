3a077c2e483d27547fd4c3f9a6238e44feded223
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-03-18 22:16:38 +0900

    2주차 과제

--- inchTocm.py
+++ inchTocm.py
@@ -1,4 +1,4 @@
 input_inch = float(input("몇 인치를 입력하실 것인가요? "))
 output_cm = 2.54 * input_inch
-print("해당 인치를 cm 로 변환시킨 결과:", output_cm, "cm 입니다 ")
+print("해당 인치를 cm 로 변환시킨 결과:",output_cm, "cm 입니다. ")
 
