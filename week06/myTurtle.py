f63266af47c751c03c50298309c874d57f1fa283
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-04-16 12:11:04 +0900

    06주차 과제 제출했습니다.

--- week06/myTurtle.py
+++ week06/myTurtle.py
@@ -1,13 +1,12 @@
 import random
 from tkinter.simpledialog import *
 
-# 1. 사용자로부터 문자열을 입력받는 함수
 def getString() :
     retStr = ''
     retStr = askstring('문자열 입력', '거북이가 쓸 문자열을 입력하세요')
     return retStr
 
-# 2. 무작위 RGB 색상을 생성하는 함수
+
 def getRGB() :
     r, g, b = 0, 0, 0
     r = random.random()
@@ -15,7 +14,7 @@ def getRGB() :
     b = random.random()
     return (r, g, b)
 
-# 3. 무작위 위치(X, Y), 각도, 글자 크기를 생성하는 함수
+
 def getXYAS(sw, sh) :
     x, y, angle, size = 0, 0, 0, 0
     x = random.randrange(-sw / 2, sw / 2)
